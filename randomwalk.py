50a9f13d2af0c8bf90e06e0aae7fbc369317b6db
Juan Rafael Álvarez Velásquez <juanra31a@gmail.com>
2014-02-20 13:25:27 -0500

    caminata aleatoria version 3

--- randomwalk.py
+++ randomwalk.py
@@ -1,17 +1,18 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import sys
 
+n_points = sys.argv[1]
 
-n_points = 100000
 a = 360*np.random.random(n_points)
-x = zeros(n_points)
-y = zeros(n_points)
-lgn = zeros(n_points)
+x = np.zeros(n_points)
+y = np.zeros(n_points)
+lgn = np.zeros(n_points)
 for i in range(n_points - 1):
     x[i+1] = x[i]+ np.cos(a[i])
     y[i+1] = y[i] + np.sin(a[i])
-    lgn[i+1] = log(i+1)
+    lgn[i+1] = np.log(i+1)
 d = np.sqrt( x**2  + y**2)
 
-
-plt.plot(range(n_points),d)
+plt.pyplot.plot(range(n_points),d)
+plt.pyplot.show()

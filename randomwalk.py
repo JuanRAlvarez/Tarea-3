6f15e881edfb13709c92c9de5d3c2f994fa7e0df
Juan Rafael Álvarez Velásquez <juanra31a@gmail.com>
2014-02-20 16:04:56 -0500

    Version numero 5

--- randomwalk.py
+++ randomwalk.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
-n_points = sys.argv[1]
+n_points = int(sys.argv[1])
 
 a = 360*np.random.random(n_points)
 x = np.zeros(n_points)
@@ -11,8 +11,14 @@ lgn = np.zeros(n_points)
 for i in range(n_points - 1):
     x[i+1] = x[i]+ np.cos(a[i])
     y[i+1] = y[i] + np.sin(a[i])
-    lgn[i+1] = np.log(i+1)
 d = np.sqrt( x**2  + y**2)
 
-plt.pyplot.plot(range(n_points),d)
-plt.pyplot.show()
+u = np.linspace(0,15,20)
+
+w = 0.5*u
+
+plt.plot(np.log(range(n_points)),np.log(d))
+plt.plot(u,w)
+plt.xlabel("Log(n_puntos)")
+plt.ylabel("Log(distancia)")
+plt.show()
